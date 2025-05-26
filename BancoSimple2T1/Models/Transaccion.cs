255dde9ffa3bd92968f614f619475cff83968bd2
unknown <kujakum723@hotmail.com>
2025-05-25 22:27:25 -0600

    agregue un metodo Esvalida que sirve para validar rapidamente las transacciones tengan los datos minimos correctos

--- BancoSimple2T1/Models/Transaccion.cs
+++ BancoSimple2T1/Models/Transaccion.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace BancoSimple2T1.Models
 {
     public class Transaccion
@@ -12,7 +14,16 @@ namespace BancoSimple2T1.Models
         public decimal Monto { get; set; }
         public DateTime Fecha { get; set; } = DateTime.Now;
         public string Descripcion { get; set; }
-        public int ? CuentaOrigenId { get; set; }
-        public int ? CuentaDestinoId { get; set; }
+        public int? CuentaOrigenId { get; set; }
+        public int? CuentaDestinoId { get; set; }
+
+        // Método para validar la transacción
+        public bool EsValida()
+        {
+            if (Monto <= 0) return false;
+            if (string.IsNullOrWhiteSpace(Descripcion)) return false;
+            if (CuentaOrigenId == null && CuentaDestinoId == null) return false;
+            return true;
+        }
     }
 }
